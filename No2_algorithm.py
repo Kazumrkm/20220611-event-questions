eea2a819d1af91902187499adaf8aca2686982ec
Kazumrkm <kazu.63.mura@icloud.com>
2022-05-27 13:31:21 +0900

    2問目

--- No2_algorithm.py
+++ No2_algorithm.py
@@ -3,6 +3,7 @@ def main():
     sorted_array = [1, 2, 3, 5, 12, 7890, 12345]
     # 探索対象の番号
     target_number = 7890
+
     # 探索実行
     target_index = serch_index(sorted_array, target_number)
     # 結果出力
@@ -11,8 +12,24 @@ def main():
 def serch_index(sorted_array, target_number):
 
     # ここから記述
+    n = len(sorted_array)
+    res = 0
 
+    for i in range(n):
+        if len(sorted_array) == 0:
+            break
 
+        mid = int(len(sorted_array)/2)
+        if sorted_array[mid] == target_number:
+            res += mid
+            return res
+
+        elif sorted_array[mid] < target_number:
+            del sorted_array[0:mid+1]
+            res += mid+1
+
+        else:
+            del sorted_array[mid:len(sorted_array)]
     # ここまで記述
 
     # 探索対象が存在しない場合、-1を返却

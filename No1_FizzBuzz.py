9dfd64788ea3a650f66aa49f2d0c9c0df28b05f4
Kazumrkm <kazu.63.mura@icloud.com>
2022-05-27 12:36:46 +0900

    3回目

--- No1_FizzBuzz.py
+++ No1_FizzBuzz.py
@@ -1,9 +1,12 @@
 for num in range(1, 101):
     string = ''
-aaa
     # ここから記述
-
-
+    if num % 3 == 0:
+        string += 'Fizz'
+    if num % 5 == 0:
+        string += 'Buzz'
+    if string == '':
+        string = str(num)
     # ここまで記述
 
     print(string)
